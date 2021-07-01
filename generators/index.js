cc8f133b04bb57dd7258b2508f5840a1e7461971
bruski <292803281@qq.com>
2021-07-02 00:17:20 +0800

    chore: init jarvis generator project

---
+++ generators/index.js
@@ -0,0 +1,29 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  async prompting() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname,
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?",
+      },
+    ]);
+    this.log("app name", answers.name);
+    this.log("cool feature", answers.cool);
+  }
+
+  writing() {
+    // this.log("cool feature", this.answers.cool);
+  }
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+function greeting() {
+  console.log('At your service, sir.')
+}
+
+module.exports = greeting    
